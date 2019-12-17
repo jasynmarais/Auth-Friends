fcd57e04f43f4e5a66ef20cc15cbf72ffb86ce61
Jasyn Marais <jasynmarais@gmail.com>
2019-12-17 15:52:56 +0200

    On form submission get token saved to localStorage

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formValues, setFormValues] = useState({ username: '', password: '' });
+
+  const handleInputChange = e => {
+    e.preventDefault();
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    });
+    console.log(e.target.value);
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', formValues)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div>
-      <form>
-        <input type='text' placeholder='Username' name='username' />
-        <input type='password' placeholder='Password' name='password' />
+      <form onSubmit={handleLogin}>
+        <input 
+        type='text'
+        onChange={handleInputChange} 
+        value={formValues.username}
+        placeholder='Username' 
+        name='username' 
+        />
+        <input 
+        type='password' 
+        onChange={handleInputChange} 
+        value={formValues.password}
+        placeholder='Password' 
+        name='password' 
+        />
         <button>Login</button>
       </form>
     </div>
